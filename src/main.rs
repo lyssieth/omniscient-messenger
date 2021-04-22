1c4c404bfb057c5d9d87c5eddc84f66368763fb6
Lyssieth <raxixor@rax.ee>
2021-04-22 09:23:41 +0300

    Basic bot skit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use log::error;
+use std::error::Error;
+use std::process::exit;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    match do_main().await {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to run bot. {:?}", e);
+            exit(1);
+        }
+    }
+}
+
+async fn do_main() -> Result<(), dyn Error> {
+    Ok(())
 }
